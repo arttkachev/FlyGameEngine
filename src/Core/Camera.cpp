ec80f07c149af0e09dac0db750a019454c1ad7b8
arttkachev <art.tkachev@bk.ru>
2021-08-02 20:04:41 +0300

    Orbit and FPS cameras implemented

---
+++ src/Core/Camera.cpp
@@ -0,0 +1,130 @@
+#include "Camera.h"
+#include "glm/gtx/transform.hpp"
+
+Camera::Camera()
+  :mPostion(glm::vec3(0.0f, 0.0f, 0.0f)),
+  mTarPos(glm::vec3(0.0f, 0.0f, 0.0f)),
+  mUp(glm::vec3(0.0f, 1.0f, 0.0f)),
+  mRight(glm::vec3(0.0f, 0.0f, 0.0f)),
+  WORLD_UP(0.0f, 1.0f, 0.0f),
+  mYaw(glm::pi<float>()),
+  mPitch(0.0f),
+  mFOV(45.0f)
+{
+
+}
+
+
+glm::mat4 Camera::getViewMatrix() const
+{
+  // just return a view matrix for the camera
+  return glm::lookAt(mPostion, mTarPos, mUp);
+}
+
+
+// Orbit Camera
+OrbitCamera::OrbitCamera()
+  : mRadius(10.0f)
+{
+
+}
+
+void OrbitCamera::rotate(float yaw, float pitch)
+{
+  // convert to radians first because glm supports rads only
+  mYaw = glm::radians(yaw);
+  mPitch = glm::radians(pitch);
+
+  // because of we use radians for pitch we use pi and 0.1f is some kind of threshold to reach very close to the max clamp value
+  // and constrain the pitch
+  mPitch = glm::clamp(mPitch, -glm::pi<float>() / 2.0f + 0.1f, glm::pi<float>() / 2.0f - 0.1f);
+
+  // once we set new pitch we need to update vectors of the camera
+  // it needs to be called every time we we rotate the camera
+  updateCameraVectors();
+}
+
+void OrbitCamera::setLookAt(const glm::vec3 & target)
+{
+  mTarPos = target;
+}
+
+void OrbitCamera::setRadius(float radius)
+{
+  mRadius = glm::clamp(radius, 2.0f, 80.0f);
+}
+
+void OrbitCamera::updateCameraVectors()
+{
+  // this method is MAGIC when camera update happens
+
+  // Spherical to Cartesian coordinates (we need to convert to coord system that supported by OpenGL)
+  // NOTE: our coordinates sys Y up not Z
+  mPostion.x = mTarPos.x + mRadius * cosf(mPitch) * sinf(mYaw);
+  mPostion.y = mTarPos.y + mRadius * sinf(mPitch);
+  mPostion.z = mTarPos.z + mRadius * cosf(mPitch) * cosf(mYaw);
+
+
+}
+
+FPSCamera::FPSCamera(glm::vec3 position, float yaw, float pitch)
+{
+  mPostion = position;
+  mYaw = yaw;
+  mPitch = pitch;
+}
+
+void FPSCamera::rotate(float yaw, float pitch)
+{
+  // convert to radians first because glm supports rads only
+  // and incrementally sets the postion
+  mYaw += glm::radians(yaw);
+  mPitch += glm::radians(pitch);
+
+  // because of we use radians for pitch we use pi and 0.1f is some kind of threshold to reach very close to the max clamp value
+  // and constrain the pitch
+  mPitch = glm::clamp(mPitch, -glm::pi<float>() / 2.0f + 0.1f, glm::pi<float>() / 2.0f - 0.1f);
+
+  // once we set new pitch we need to update vectors of the camera
+  // it needs to be called every time we we rotate the camera
+  updateCameraVectors();
+}
+
+void FPSCamera::setPostion(const glm::vec3 & position)
+{
+  mPostion = position;
+}
+
+void FPSCamera::move(const glm::vec3 & offsetPos)
+{
+  // just simple moving a camera with offset incrementally
+  mPostion += offsetPos;
+  // after each camera move, as always, we need to update camera vectors
+  // Up/Right/Forward should always be correct, so, we update camera vectors evert time once a user moved the camera
+  updateCameraVectors();
+}
+
+void FPSCamera::updateCameraVectors()
+{
+  // this method is MAGIC when camera update happens
+
+  // Spherical to Cartesian coordinates (we need to convert to coord system that supported by OpenGL)
+  // NOTE: our coordinates sys Y up not Z
+
+  // calculate the view direction vector based on yaw and pitch angles (roll not considered)
+  glm::vec3 look{};
+  look.x = cosf(mPitch) * sinf(mYaw);
+  look.y = sinf(mPitch);
+  look.z = cosf(mPitch) * cosf(mYaw);
+
+  // camera look (where camera watches is normalized a vector got here (look)
+  mLook = glm::normalize(look);
+
+  // Re-calculate Right and Up vector. For simplicity the Right vector will be assumed horizontal
+  // w.r.t. the world's Up vector  
+  mRight = glm::normalize(glm::cross(mLook, WORLD_UP)); // calculate Right Vector with Cross Product
+  mUp = glm::normalize(glm::cross(mRight, mLook)); // calculate Up Vector with Cross Product
+
+  // update Target Position based on calculations
+  mTarPos = mPostion + mLook; // positon of the camera added to a camera look vector
+}
--- src/Main.cpp
+++ src/Main.cpp
@@ -6,6 +6,7 @@
 #include "glm/gtc/matrix_transform.hpp"
 #include "Shaders/ShaderProgram.h"
 #include "Core/Texture2D.h"
+#include "Core/Camera.h"
 
 // ptr to a main window
 // create a window. We moved to global data to have an access to our main window from different scopes and functions 
@@ -21,16 +22,46 @@ bool gDrawStats = false;
 bool gWireframeMode = false;
 bool gRotateCubeToRight = false;
 bool gRotateCubeToLeft = false;
+bool gUseFPSCamera = false;
+
+// create an Orbit Camera
+OrbitCamera orbitCamera;
+
+// we are going to manipulate by Yaw and Pitch from the main app
+float gYaw{};
+float gPitch{};
+float gRadius = 10.0f;
+
+// create an FPSCamera
+FPSCamera fpsCamera(glm::vec3(0.0f, 0.0f, -5.0f));
+const double ZOOM_SENSITIVITY = -3.0f;
+const float MOVE_SPEED = 5.0f; //units per seconds
+const float MOUSE_SENSITIVITY = 0.1f;
+
+// position of the cube 
+// - 5.0f in fron of the camera
+glm::vec3 cubePos = glm::vec3(0.0f, 0.0f, -5.0f);
+glm::vec3 floorPos{}; // floor position
 
 // textures
 //const string defaultTexture = "./Textures/defaultTexture.png";
 const string crateTexture = "./Textures/crate.jpg";
+const string gridTexture = "./Textures/grid.jpg";
 
+// glfw handlers
 // callback function for specific key bindings
 void glfw_onKey(GLFWwindow* window, int key, int scancode, int action, int mode);
 
 // callback function on window resized
 void glfw_onFrameWindowBufferResized(GLFWwindow* window, int width, int height);
+
+// mouse cursor handler
+void glfw_onMouseMove(GLFWwindow* window, double posX, double posY);
+
+// mouse scroll handler
+void glfw_onMouseScroll(GLFWwindow* window, double deltaX, double deltaY);
+
+void update(double elapsedTime);
 
 // a function to show frame stats
 void showFrameStats(GLFWwindow* window, bool drawStats);
@@ -70,7 +101,7 @@ int main() // entry renderer point
   //  0, 2, 3  // trig 1
   //};
 
-  // we wanna draw 3D cube
+  // we wanna draw 3D cube. Specify vertex positions in the array to get a cube
   
   GLfloat cube3D[] = {
     // position		 // tex coords
@@ -121,11 +152,7 @@ int main() // entry renderer point
       -1.0f, -1.0f, 1.0f, 0.0f, 1.0f,
       -1.0f, -1.0f, -1.0f, 0.0f, 0.0f,
       1.0f, -1.0f, -1.0f, 1.0f, 0.0f,
-  };
-
-  // position of the cube 
-  // - 5.0f in fron of the camera
-  glm::vec3 cubePos = glm::vec3(0.0f, 0.0f, -5.0f); 
+  };     
 
   //// color
   //GLfloat vert_color[] = {
@@ -237,6 +264,9 @@ int main() // entry renderer point
   Texture2D crateTextureInstance{};
   crateTextureInstance.loadTexture(crateTexture);
 
+  Texture2D gridTextureInstance{};
+  gridTextureInstance.loadTexture(gridTexture);
+
   // rotate 3D cube
   float cubeAngle = 0.0f;
   double lastTime = glfwGetTime();
@@ -253,9 +283,9 @@ int main() // entry renderer point
     float deltaTime = currentTime - lastTime;
 		
 		// Quering any inputs (from keyboard, mouse and etc...)
-		glfwPollEvents();		
-
-		// What kind of things we want to clear (in our case this is COLOR_BUFFER | DEPTH_BUFFER)
+		glfwPollEvents();
+    
+    // What kind of things we want to clear (in our case this is COLOR_BUFFER | DEPTH_BUFFER)
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     // binding is required when using multiple textures    
@@ -263,11 +293,12 @@ int main() // entry renderer point
     //crateTextureInstance.bind(1); // for multiple texture a texUnit allows to bind a single texture to different units
     // that allows to blend multiple textures
     // OpenGL can only have 1 texture bound. We handle it with texUnit that's being used for multiple texturing
-    // In our frag shader we can reference to both those texture units  
-
+    // In our frag shader we can reference to both those texture units   
+    
     // actual 3D cube rotation 50.0f is the speed of the rotation
     // rotate cubeAngle with delta time
-    if (gRotateCubeToRight)
+    // !!!!!! Once we created a camera we comment this
+    /*if (gRotateCubeToRight)
     {
       cubeAngle += (float)(deltaTime * 50.0f);
     }
@@ -275,14 +306,15 @@ int main() // entry renderer point
     if (gRotateCubeToLeft)
     {
       cubeAngle += (float)(deltaTime * -50.0f);
-    }
+    }*/
     
 
     // wrap around 360
-    if (cubeAngle >= 360.0f)
+    // !!!!!! Once we created a camera we comment this
+    /*if (cubeAngle >= 360.0f)
     {
       cubeAngle = 0.0f;
-    }
+    }*/
 
     // matrices to transform vertices to clip space
     glm::mat4 model, view, projection;
@@ -292,23 +324,73 @@ int main() // entry renderer point
     // args (4by4 matrix - we pass model because it' cleared above each tick, target postion)
     // and we're also going to set up rotation , so, we multiply by rotation
     // args (4by4 matrix, angle, axis we're going to rotate around). We wanna rotate aroung y - axis
-    model = glm::translate(model, cubePos) * glm::rotate(model, glm::radians(cubeAngle), glm::vec3(0.0f, 0.1, 0.0));
+
+    // !!!!!! Once we created a camera we don't want to rotate the cube anymore. We leave only translate part below
+    //model = glm::translate(model, cubePos) * glm::rotate(model, glm::radians(cubeAngle), glm::vec3(0.0f, 0.1, 0.0));
+    model = glm::translate(model, cubePos);
 
     // setup view matrix. It creates our view matrix we pass in our vertex shader
-    glm::vec3 cameraPos(0.0f, 0.0f, 0.0f);
-    glm::vec3 targetPos(0.0f, 0.0f, -1.0f); // because we set our cube in -5.0f pos. Camera has "eyes" in a negative axis to see a cube
-    glm::vec3 upVec(0.0f, 1.0f, 0.0f);
+    // !!!!!! Once we created a camera we comment this
+    //glm::vec3 cameraPos(0.0f, 0.0f, 0.0f);
+    //glm::vec3 targetPos(0.0f, 0.0f, -1.0f); // because we set our cube in -5.0f pos. Camera has "eyes" in a negative axis to see a cube
+    //glm::vec3 upVec(0.0f, 1.0f, 0.0f);
     // lookAt(3 vec3 vectors as args: pos of camera, where it's looking, up vector). Creates "eyes" of the camera
     // glm makes all job for us with lookat method
-    view = glm::lookAt(cameraPos, targetPos, upVec);
+
+    // !!!!!! Once we created a camera we don't want to explicitly set a view matrix in a hard-coded way
+    // and use data from mouse cursor position and click button callback
+    //view = glm::lookAt(cameraPos, targetPos, upVec);
+
+    //view = orbitCamera.getViewMatrix();
+
+    // Camera settings
+    const float fov = 45.0f;
+    const float nearPlane = 0.1f;
+    const float farPlane = 100.0f;
+
+    if (gUseFPSCamera)
+    {      
+      // call update method after PollEvents to register on pressed buttons
+      // update methods for fpsCamera to move it
+      update(deltaTime);
+
+      view = fpsCamera.getViewMatrix();
+      projection = glm::perspective(glm::radians(fpsCamera.getFOV()), (float)gWindowWidth / (float)gWindowHeight, nearPlane, farPlane);
+
+      // Hides and grabs cursor, unlimited movement
+      glfwSetInputMode(gWindow, GLFW_CURSOR, GLFW_CURSOR_DISABLED); // disable the cursor      
+    }
+    else
+    {
+      view = orbitCamera.getViewMatrix();
+      // actually there is no need to do it every frame in the loop because a user might not change a camera every frame by moving a mouse      
+      orbitCamera.setLookAt(cubePos); // target (where camera looks is a cubePose)
+      orbitCamera.rotate(gYaw, gPitch); // setted from mouse cursor position
+      orbitCamera.setRadius(gRadius); // setted in mouse callback function
+
+      // create projection matrix (perspective not orthogonal)
+      // args: (field of view, aspect ration, near plane, far plane)
+      // glm makes all job for us with perspective method
+      
+      projection = glm::perspective(glm::radians(fov), (float)gWindowWidth / (float)gWindowHeight, nearPlane, farPlane);
+
+      // Hides and grabs cursor, unlimited movement
+      glfwSetInputMode(gWindow, GLFW_CURSOR, GLFW_CURSOR_NORMAL); // disable the cursor      
+    }
+    
+
+    // actually there is no need to do it every frame in the loop because a user might not change a camera every frame by moving a mouse
+    //orbitCamera.setLookAt(cubePos); // target (where camera looks is a cubePose)
+    //orbitCamera.rotate(gYaw, gPitch); // setted from mouse cursor position
+    //orbitCamera.setRadius(gRadius); // setted in mouse callback function
 
     // create projection matrix (perspective not orthogonal)
     // args: (field of view, aspect ration, near plane, far plane)
     // glm makes all job for us with perspective method
-    const float fov = 45.0f;
-    const float nearPlane = 0.1f;
-    const float farPlane = 100.0f;
-    projection = glm::perspective(glm::radians(fov), (float)gWindowWidth / (float)gWindowHeight, nearPlane, farPlane);
+    //projection = glm::perspective(glm::radians(fov), (float)gWindowWidth / (float)gWindowHeight, nearPlane, farPlane);
+
+    // we use a fpsCamera, so, commented the hard-coded line above and use this
+    // projection = glm::perspective(glm::radians(fpsCamera.getFOV()), (float)gWindowWidth / (float)gWindowHeight, nearPlane, farPlane);
 
     ////////////////DRAWING_TRIANGLE///////////////////////////
     // activate shader program
@@ -346,10 +428,27 @@ int main() // entry renderer point
     glBindVertexArray(vao);
 
     //// how to draw a triangle (wining order clockwise)
+    // this is actual drawing of our vertices from the array
     glDrawArrays(GL_TRIANGLES, 0, 36); // args: what kind of primitives our data makes up, start position, number of vertices (3 for a triangle / 6 for a quad)
 
     // since we use index buffer to prevent from double vertices in drawing quads we use another method different from glDrawArrays
     //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); // args: what kind of primitives our data makes up, number of vertices, type for indices, offset (no stride in front of the array)
+
+    // FLOOR
+    // after drawing a cube we make a texture of the grid "active" and draw a floor (it's made in sequence)
+    // if bind a texture above after binding the texture of the cube both objects (cube and floor) will be drawn with one texture (last bound)
+    gridTextureInstance.bind(0); // we bind to 0 texUnit because we reuse a cube with another texture to get a floor    
+    floorPos.y = -1.0f; // we wanna draw a floor a little bit lower than a cube
+
+    // Position and render the floor (a squashed and scaled cube!)
+    // Make the floor texture "active" in the shaders
+    // model matrix of the floor (translate) multiplied by scale to flat our cube and get it as a floor. We reuse cube to get a floor
+    model = glm::translate(model, floorPos) * glm::scale(model, glm::vec3(10.0f, 0.01f, 10.0f));
+
+    // we also need to tell our shader to draw the floor one more time and draw our floor (vertices) one more time
+    shaderProgram.setUniform("model", model);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
+
 
     // after each draw we must undind our vertext array object
     glBindVertexArray(0);
@@ -394,29 +493,30 @@ void glfw_onKey(GLFWwindow* window, int key, int scancode, int action, int mode)
 		glfwSetWindowShouldClose(window, GL_TRUE);
 	}
 
-  // rotate cube to right
-  if (key == GLFW_KEY_RIGHT && action == GLFW_PRESS)
-  {
-    gRotateCubeToRight = true;
-  } 
+  // !!!!!!!!!!!!! we comment this because now we use a camera
+  //// rotate cube to right
+  //if (key == GLFW_KEY_RIGHT && action == GLFW_PRESS)
+  //{
+  //  gRotateCubeToRight = true;
+  //} 
 
-  // stop right cube rotation
-  if (key == GLFW_KEY_RIGHT && action == GLFW_RELEASE)
-  {
-    gRotateCubeToRight = false;
-  }
+  //// stop right cube rotation
+  //if (key == GLFW_KEY_RIGHT && action == GLFW_RELEASE)
+  //{
+  //  gRotateCubeToRight = false;
+  //}
 
-  // rotate cube to left
-  if (key == GLFW_KEY_LEFT && action == GLFW_PRESS)
-  {
-    gRotateCubeToLeft = true;
-  }
+  //// rotate cube to left
+  //if (key == GLFW_KEY_LEFT && action == GLFW_PRESS)
+  //{
+  //  gRotateCubeToLeft = true;
+  //}
 
-  // stop left cube rotation
-  if (key == GLFW_KEY_LEFT && action == GLFW_RELEASE)
-  {
-    gRotateCubeToLeft = false;
-  }
+  //// stop left cube rotation
+  //if (key == GLFW_KEY_LEFT && action == GLFW_RELEASE)
+  //{
+  //  gRotateCubeToLeft = false;
+  //}
 
   // draw stats
 	if (key == GLFW_KEY_F5 && action == GLFW_PRESS)
@@ -439,6 +539,17 @@ void glfw_onKey(GLFWwindow* window, int key, int scancode, int action, int mode)
       glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
     }
   }
+
+  // switch camera
+  if (key == GLFW_KEY_F1 && action == GLFW_PRESS)
+  {
+    gUseFPSCamera = false;    
+  }
+
+  if (key == GLFW_KEY_F2 && action == GLFW_PRESS)
+  {
+    gUseFPSCamera = true;
+  }
 }
 
 void glfw_onFrameWindowBufferResized(GLFWwindow * window, int width, int height)
@@ -449,6 +560,77 @@ void glfw_onFrameWindowBufferResized(GLFWwindow * window, int width, int height)
   // a method that convers from OpenGL 2D coord to Screen viewport and resize our viewport
   // args (lower left corner of the coords (x), lower left corner of the coords (y), screen width (1024), screen height (768))
   glViewport(0, 0, gWindowWidth, gWindowHeight);
+}
+
+void glfw_onMouseMove(GLFWwindow* window, double posX, double posY)
+{
+  // keep storing a last mouse position and substract/add from/to it below to update the camera orbit
+  static glm::vec2 lastMousePos = glm::vec2(0, 0);
+
+  // update angles based on LETT MOUSE BUTTON input to orbit around a target point
+  if (glfwGetMouseButton(gWindow, GLFW_MOUSE_BUTTON_LEFT) == 1)
+  {
+
+    // each pixel represents a quarter of a degree rotation (this is our MOUSE_SENSITIVITY)
+    // without MOUSE_SENSITIVITY movement of the camera will be too fast
+    gYaw -= ((float)posX - lastMousePos.x) * MOUSE_SENSITIVITY;
+    gPitch += ((float)posY - lastMousePos.y) * MOUSE_SENSITIVITY;
+  }
+
+  if (glfwGetMouseButton(gWindow, GLFW_MOUSE_BUTTON_RIGHT) == 1)
+  {
+    // change orbit camera radius with the RIGHT MOUSE BUTTON (Zooming)
+    float dx = 0.01f * ((float)posX - lastMousePos.x);
+    float dy = 0.01f * ((float)posY - lastMousePos.y);
+    // actual zooming
+    gRadius += dx - dy;
+  }
+  // set current position as last mouse position
+  lastMousePos.x = (float)posX;
+  lastMousePos.y = (float)posY;
+}
+
+// called when mouse wheel rotated
+void glfw_onMouseScroll(GLFWwindow * window, double deltaX, double deltaY)
+{
+  double fov = fpsCamera.getFOV() + deltaY * ZOOM_SENSITIVITY;
+  fov = glm::clamp(fov, 1.0, 120.0);
+  fpsCamera.setFOV((float)fov);
+}
+
+void update(double elapsedTime)
+{
+  // Camera orientation
+  double mouseX{}, mouseY{};
+
+  // Get the current mouse cursor position delta
+  glfwGetCursorPos(gWindow, &mouseX, &mouseY);
+
+  // Rotate the camera the difference in mouse distance from the center screen.  Multiply this delta by a speed scaler
+  fpsCamera.rotate((float)(gWindowWidth / 2.0 - mouseX) * MOUSE_SENSITIVITY, (float)(gWindowHeight / 2.0 - mouseY) * MOUSE_SENSITIVITY);
+
+  // Clamp mouse cursor to center of screen
+  glfwSetCursorPos(gWindow, gWindowWidth / 2.0, gWindowHeight / 2.0);
+
+  // Camera FPS movement
+
+  // Forward/backward
+  if (glfwGetKey(gWindow, GLFW_KEY_W) == GLFW_PRESS)
+    fpsCamera.move(MOVE_SPEED * (float)elapsedTime * fpsCamera.getLook());
+  else if (glfwGetKey(gWindow, GLFW_KEY_S) == GLFW_PRESS)
+    fpsCamera.move(MOVE_SPEED * (float)elapsedTime * -fpsCamera.getLook());
+
+  // Strafe left/right
+  if (glfwGetKey(gWindow, GLFW_KEY_A) == GLFW_PRESS)
+    fpsCamera.move(MOVE_SPEED * (float)elapsedTime * -fpsCamera.getRight());
+  else if (glfwGetKey(gWindow, GLFW_KEY_D) == GLFW_PRESS)
+    fpsCamera.move(MOVE_SPEED * (float)elapsedTime * fpsCamera.getRight());
+
+  // Up/down
+  if (glfwGetKey(gWindow, GLFW_KEY_Z) == GLFW_PRESS)
+    fpsCamera.move(MOVE_SPEED * (float)elapsedTime * fpsCamera.getUp());
+  else if (glfwGetKey(gWindow, GLFW_KEY_X) == GLFW_PRESS)
+    fpsCamera.move(MOVE_SPEED * (float)elapsedTime * -fpsCamera.getUp());
 }
 
  // show fram stats implementation
@@ -566,6 +748,12 @@ bool initOpenGL()
   // Register a callback function
   glfwSetKeyCallback(gWindow, glfw_onKey);
 
+  // Mouse cursor callback. Tell glfw about mouse move callback
+  glfwSetCursorPosCallback(gWindow, glfw_onMouseMove);
+
+  // Mouse Scroll callback
+  glfwSetScrollCallback(gWindow, glfw_onMouseScroll);  
+
   // We need to initialize that (according to documentation) to set GLEW properly 
   glewExperimental = GL_TRUE;
 
@@ -585,6 +773,9 @@ bool initOpenGL()
   // enable depth test vertices to draw on top vertices that are closer to the camera and behind that are farther from the camera
   // args: (what to enable)
   glEnable(GL_DEPTH_TEST);
-  return true;
 
+  // Hides and grabs cursor, unlimited movement
+  // glfwSetInputMode(gWindow, GLFW_CURSOR, GLFW_CURSOR_NORMAL); // disable the cursor
+  glfwSetCursorPos(gWindow, gWindowWidth / 2.0, gWindowHeight / 2.0); // set cursor position in the center of the screen on init
+  return true;
 }
